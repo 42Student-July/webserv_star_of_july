f5d35dc752c7df4d6fb5b433c6bcb2e1c3f173f3
bayamasa <mapi0213mgt@gmail.com>
2022-07-12 15:20:44 +0900

    テスト追加

--- src/response/sandbox/stringcommand.cpp
+++ src/response/sandbox/stringcommand.cpp
@@ -1,15 +1,26 @@
 #include <string>
 #include <iostream>
 
-int main(void)
+
+void parse(std::string path)
 {
 	std::string dir;
 	std::string file;
 	
-	std::string path = "hello/path/index.html";
 	int last_slash = path.find_last_of("/");
-	dir = path.substr(0, last_slash);
+	dir = path.substr(0, last_slash + 1);
 	file = path.substr(last_slash + 1);
 	std::cout << "dir: " << dir << std::endl;
 	std::cout << "file: " << file << std::endl;
+}
+
+int main(void)
+{
+	std::string path = "/hello/path/index.html";
+	std::string no_dir = "/trend";
+	std::string index = "/";
+	std::string no_file = "/hello/world/";
+	// parse(no_dir);
+	// parse(index);
+	parse(no_file);
 }
