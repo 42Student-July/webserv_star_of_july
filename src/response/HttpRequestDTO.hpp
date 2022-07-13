9175024268c7917feec6e33c4fcb8385aca98feb
bayamasa <mapi0213mgt@gmail.com>
2022-07-13 20:40:29 +0900

    request dtoに追加

--- src/response/HttpRequestDTO.hpp
+++ src/response/HttpRequestDTO.hpp
@@ -10,7 +10,15 @@ struct HttpRequestDTO
 	std::string version;
 	std::string connection;
 	std::string body;
-	
+
+	std::string authorization;
+	std::string content_length;
+	std::string content_type;
+	std::string accept;
+	std::string forwarded;
+	std::string referer;
+	std::string user_agent;
+	std::string x_forwarded_for;
 	// TODO: 後で消す
 	std::string file;
 };
