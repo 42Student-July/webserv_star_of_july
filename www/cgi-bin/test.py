6caf29ce621b654188df49a56e3a8c0bf23d9cf2
yuki <yuki@yukis-MacBook-Air.local>
2022-07-21 17:28:29 +0900

    chore: cgiを統合しました（ブラウザ等から未確認）

---
+++ www/cgi-bin/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+print("Content-type: text/html\r\n")
+print("Location: http://127.0.0.1/\r\n")
+print("StatusCode: 200\r\n")
+print("\r\n\r\n")
+print("<!DOCTYPE html>")
+print("<html>")
+print("<head><title>sample html</title></head>")
+print("<body>")
+print("<h1>hello test.py</h1>")
+print("</body></html>")
