08d4724dee77c5fc3137bf4e59e44adf287f52b3
yuki <yuki@yukis-MacBook-Air.local>
2022-07-22 16:54:43 +0900

    fix: compile error

--- src/config/ConfigParser.cpp
+++ src/config/ConfigParser.cpp
@@ -1,5 +1,14 @@
 #include "ConfigParser.hpp"
 
+const unsigned int ConfigParser::BIT_FLAG_LISTEN = (1 << 0); // 0000 0000 0000 0001
+const unsigned int ConfigParser::BIT_FLAG_HOST = (1 << 1); // 0000 0000 0000 0010
+const unsigned int ConfigParser::BIT_FLAG_S_ROOT = (1 << 2); // 0000 0000 0000 0100
+const unsigned int ConfigParser::BIT_FLAG_BODY_LIMIT = (1 << 3); // 0000 0000 0000 1000
+const unsigned int ConfigParser::BIT_FLAG_ = (1 << 4); // 0000 0000 0001 0000
+const unsigned int ConfigParser::BIT_FLAG_5 = (1 << 5); // 0000 0000 0010 0000
+const unsigned int ConfigParser::BIT_FLAG_6 = (1 << 6); // 0000 0000 0100 0000
+const unsigned int ConfigParser::BIT_FLAG_7 = (1 << 7); // 0000 0000 1000 0000
+
 template <typename T>
 void print_vector(std::vector<T> vec) {
   typename std::vector<T>::iterator it = vec.begin();
--- src/config/ConfigParser.hpp
+++ src/config/ConfigParser.hpp
@@ -33,14 +33,14 @@ class ConfigParser {
  private:
   std::vector<ServerConfig> serverconfigs_;
 
-  const unsigned int BIT_FLAG_LISTEN = (1 << 0); // 0000 0000 0000 0001
-  const unsigned int BIT_FLAG_HOST = (1 << 1); // 0000 0000 0000 0010
-  const unsigned int BIT_FLAG_S_ROOT = (1 << 2); // 0000 0000 0000 0100
-  const unsigned int BIT_FLAG_BODY_LIMIT = (1 << 3); // 0000 0000 0000 1000
-  const unsigned int BIT_FLAG_ = (1 << 4); // 0000 0000 0001 0000
-  const unsigned int BIT_FLAG_5 = (1 << 5); // 0000 0000 0010 0000
-  const unsigned int BIT_FLAG_6 = (1 << 6); // 0000 0000 0100 0000
-  const unsigned int BIT_FLAG_7 = (1 << 7); // 0000 0000 1000 0000
+  static const unsigned int BIT_FLAG_LISTEN; // 0000 0000 0000 0001
+  static const unsigned int BIT_FLAG_HOST; // 0000 0000 0000 0010
+  static const unsigned int BIT_FLAG_S_ROOT; // 0000 0000 0000 0100
+  static const unsigned int BIT_FLAG_BODY_LIMIT; // 0000 0000 0000 1000
+  static const unsigned int BIT_FLAG_; // 0000 0000 0001 0000
+  static const unsigned int BIT_FLAG_5; // 0000 0000 0010 0000
+  static const unsigned int BIT_FLAG_6; // 0000 0000 0100 0000
+  static const unsigned int BIT_FLAG_7; // 0000 0000 1000 0000
 
   // locationのparse
   void parseLocation(LocationConfig &location,
